0bca01dd80cc704f8464598b3e9b3adcf66b2e05
rodrigoxaviertaborda <rodrigo.xaviertaborda@iut-rodez.fr>
2025-03-03 14:27:47 +0100

    importation du modèle MVC (yasmf)

---
+++ all_users/services/UsersService.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace services;
+
+
+use PDO;
+use PDOStatement;
+
+/**
+ * The users service class
+ */
+class UsersService
+{
+    /**
+     * Find users by criteria
+     *
+     * @param PDO $pdo the pdo object
+     * @param string $likeUsername the string the username should contain
+     * @param int $statusId the status id
+     * @return PDOStatement the statement referencing the result set
+     */
+    public function findUsersByUsernameAndStatus(PDO $pdo, string $likeUsername, int $statusId): PDOStatement
+    {
+        $sql = "select users.id as user_id, username, email, s.name as status, s.id as status_id 
+            from users join status s on users.status_id = s.id 
+            where username like :likeUsername and status_id = :statusId order by username";
+        $search_stmt = $pdo->prepare($sql);
+        $search_stmt->execute(['likeUsername' => $likeUsername, 'statusId' => $statusId]);
+        return $search_stmt;
+    }
+
+}
