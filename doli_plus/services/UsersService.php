cb14650f79adf78d152a39346ab34348ea483a37
rodrigoxaviertaborda <rodrigo.xaviertaborda@iut-rodez.fr>
2025-03-09 12:21:49 +0100

    avancement des pages en statique + ajout d'un commentaire expliquant le but de "UsersService" dans notre cas

--- doli_plus/services/UsersService.php
+++ doli_plus/services/UsersService.php
@@ -3,12 +3,18 @@
 
 namespace services;
 
-
 use PDO;
 use PDOStatement;
 
 /**
- * The users service class
+ * Dans le modèle yasmf, cette classe sert à intéragir avec la BD.
+ * C'est la classe contenant les différentes méthodes récupérant les données, en
+ * faisant des requêtes.
+ * -----------------------------------------------------------------------------
+ * Or, étant donnné que dans notre projet aucune intéraction avec une BD va avoir
+ * lieu.
+ * Cette classe va contenir toutes les requêtes (HTTP) à l'API afin de récupérer
+ * les données souhaitées.
  */
 class UsersService
 {
