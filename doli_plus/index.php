c0e828846528bf32fc3ba6d278ce534144e20159
V5lMG <114313700+V5lMG@users.noreply.github.com>
2025-03-15 17:49:23 +0100

    debug de la page de connexion

--- doli_plus/index.php
+++ doli_plus/index.php
@@ -3,10 +3,7 @@ const PREFIX_TO_RELATIVE_PATH = "/doli_plus";
 require $_SERVER[ 'DOCUMENT_ROOT' ] . PREFIX_TO_RELATIVE_PATH . '/lib/vendor/autoload.php';
 
 use application\DefaultComponentFactory;
-use yasmf\DataSource;
 use yasmf\Router;
-
-//$data_source = null; // STUB
 
 $router = new Router(new DefaultComponentFactory()) ;
 try {
--- doli_plus/views/auth_page.php
+++ doli_plus/views/auth_page.php
@@ -1,16 +1,18 @@
 <!doctype html>
 <html lang="fr">
-    <head>
-        <meta charset="utf-8">
-        <title>DoliPlus - Connexion</title>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-        <link rel="stylesheet" href="static/css/auth.css">
-    </head>
-    <body class="body bodylogin">
-        <div class="login_center center">
-            <div class="login_vertical_align">
-                <form id="login" name="login" method="post" action="../index.php">
+<head>
+    <meta charset="utf-8">
+    <title>DoliPlus</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link rel="stylesheet" href="static/css/auth.css">
+</head>
+<body>
+    <!-- Ligne de fond -->
+    <div class="login_center center" style="background-size: cover; background: linear-gradient(4deg, rgb(60,70,100) 52%, rgb(240,240,240) 52.1%) no-repeat fixed center center;">
+        <div class="login_vertical_align">
+            <form id="login" name="login" method="post" action="doli_plus.php">
+                <div class="login_table">
                     <div id="login_line1">
                         <div id="login_left">
                             <img alt="" src="static/img/doliplus_logo.png" id="img_logo" />
@@ -34,24 +36,18 @@
                     </div>
                     <div id="login_line2">
                         <div id="login-submit-wrapper">
-                            <input hidden name="action" value="login"/>
-                            <input hidden name="controller" value="Auth"/>
-                            <input id="button-connection" type="submit" class="button" value="Se connecter"/>
+                            <input type="submit" class="button" value="Se connecter" />
                         </div>
                         <br>
                     </div>
-                    <br><div class="center" style="margin-top: 5px;"><a class="alogin" href="/user/passwordforgotten.php">Mot de passe oublié ?</a></div>
-
-                    <!-- Message d'erreur en cas d'échec de la connexion -->
-                    <?php use yasmf\HttpHelper;
-
-                    if (HttpHelper::getParam('error') === 'true') : ?>
-                        <div class="alert alert-danger mt-3" role="alert">
-                            Identifiants incorrects. Veuillez réessayer.
-                        </div>
-                    <?php endif; ?>
-                </form>
+                    <br><div class="center" style="margin-top: 5px;"><a class="alogin" href="/G2024-43-SAE/htdocs/user/passwordforgotten.php">Mot de passe oubli&eacute; ?</a></div>
+                </div>
+            </form>
+            <!-- SubTitle with version -->
+            <div class="login_table_title center" title="DoliPlus 1.0.0">
+                <a class="login_table_title" href="https://www.dolibarr.org" target="_blank" rel="noopener noreferrer external">DoliPlus 1.0.0</a>
             </div>
         </div>
+    </div>
     </body>
 </html>
