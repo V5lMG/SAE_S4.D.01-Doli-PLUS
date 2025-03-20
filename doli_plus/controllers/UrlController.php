4abff117e51ec4e54e93473b36b564c0d795cb4f
Makflip0 <mathiascambon3@gmail.com>
2025-03-20 14:47:10 +0100

    Ajout d'une sécurité si user tente d'aller sur l'app sans être authentifier mais avec un url

--- doli_plus/controllers/UrlController.php
+++ doli_plus/controllers/UrlController.php
@@ -1,12 +1,15 @@
 <?php
 namespace controllers;
 
+use services\AuthService;
 use yasmf\View;
 
 class UrlController
 {
     public function index(): View
     {
+        AuthService::checkAuthentication();
+
         return new View("views/config_url");
     }
 }
