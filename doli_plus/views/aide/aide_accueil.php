f2f776106b1dad41b7e54361219fee4d6973a495
erwan.thierry <erwan.thierry@iut-rodez.fr>
2025-03-30 12:19:46 +0200

    Ajout des informations dans les pages d'aide correspondant à leur page - 4/7 de fait

--- doli_plus/views/aide/aide_accueil.php
+++ doli_plus/views/aide/aide_accueil.php
@@ -32,6 +32,51 @@ if (session_status() === PHP_SESSION_NONE) {
                     </div>
                 </div>
 
+                <!-- Information de la page -->
+                <div class="row justify-content-center">
+                    <div class="col-12 col-md-8 ">
+                        <div class="p-4 border rounded shadow-sm bg-light">
+
+                            <!-- Section Navigation -->
+                            <h3>Navigation :</h3>
+                            <ul>
+                                <li>
+                                    <strong>Menu de gauche (sidebar) :</strong> Vous trouverez ici les différentes sections de l'application :
+                                    <ul>
+                                        <li><strong>Accueil :</strong> <u>Vous êtes ici !</u> Cette page vous donne une vue d'ensemble de l'application et de ses fonctionnalités.</li>
+                                        <li><strong>Liste des notes de frais :</strong> Consultez ici les listes des notes de frais.</li>
+                                        <li><strong>Statistiques :</strong> Visualisez les statistiques relatives aux notes de frais.</li>
+                                        <li><strong>Historique des factures d'un fournisseur :</strong> Consultez l'historique des factures d'un fournisseur.</li>
+                                        <li><strong>Palmarès :</strong> Accédez au palmarès des fournisseurs ayant le plus grand chiffre d'affaires.</li>
+                                    </ul>
+                                </li>
+                            </ul>
+
+                            <!-- Section Profils des utilisateurs -->
+                            <h3>Profils d'utilisateurs :</h3>
+
+                            <p>L'accès à l'application est limité en fonction de votre profil :</p>
+                            <ul>
+                                <li><strong>Administrateurs :</strong> Accès complet à toutes les fonctionnalités de l'application.</li>
+                                <li><strong>Gestionnaires des notes de frais :</strong> Accès à la gestion des notes de frais uniquement.</li>
+                                <li><strong>Gestionnaires des achats :</strong> Accès à la consultation et à la gestion des factures et des fournisseurs uniquement.</li>
+                                <li><strong>Autres utilisateurs :</strong> Aucun accès aux fonctionnalités.</li>
+                            </ul>
+
+                            <!-- Section Nom utilisateur et Déconnexion -->
+                            <h3>Nom utilisateur, Déconnexion :</h3>
+                            <p>Votre nom d'utilisateur est affiché en bas à gauche de l'écran. Lors du clique sur celui-ci, il est possible de se déconnecter de l'application.</p>
+
+                            <!-- Section Conseils supplémentaires -->
+                            <h3>Conseils supplémentaires :</h3>
+                            <ul>
+                                <li>N'hésitez pas à explorer les différentes sections de l'application pour vous familiariser avec ses fonctionnalités.</li>
+                                <li>Si vous avez des questions, il y a l'aide associé à chaque page en bas à gauche de l'écran.</li>
+                            </ul>
+
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
--- doli_plus/views/aide/aide_noteFrais.php
+++ doli_plus/views/aide/aide_noteFrais.php
@@ -32,6 +32,63 @@ if (session_status() === PHP_SESSION_NONE) {
                     </div>
                 </div>
 
+                <!-- Information de la page -->
+                <div class="row justify-content-center">
+                    <div class="col-12 col-md-8 ">
+                        <div class="p-4 border rounded shadow-sm bg-light">
+
+                            <!-- Section Navigation -->
+                            <h3>Navigation :</h3>
+                            <ul>
+                                <li>
+                                    <strong>Menu de gauche (sidebar) :</strong> Vous trouverez ici les différentes sections de l'application :
+                                    <ul>
+                                        <li><strong>Accueil :</strong> Cette page vous donne une vue d'ensemble de l'application et de ses fonctionnalités.</li>
+                                        <li><strong>Liste des notes de frais :</strong> <u> Vous êtes ici !</u> Consultez ici les listes des notes de frais.</li>
+                                        <li><strong>Statistiques :</strong> Visualisez les statistiques relatives aux notes de frais.</li>
+                                        <li><strong>Historique des factures d'un fournisseur :</strong> Consultez l'historique des factures d'un fournisseur.</li>
+                                        <li><strong>Palmarès :</strong> Accédez au palmarès des fournisseurs ayant le plus grand chiffre d'affaires.</li>
+                                    </ul>
+                                </li>
+                            </ul>
+
+                            <!-- Section Filtre des notes de frais -->
+                            <h3>Filtrage des notes de frais</h3>
+                            <p>Vous pouvez filtrer les notes de frais en utilisant les options suivantes :</p>
+                            <ul>
+                                <li><strong>Employé :</strong> Sélectionnez un employé spécifique.</li>
+                                <li><strong>Type de la note :</strong> Choisissez un type de note de frais (par exemple, "Tous").</li>
+                                <li><strong>Référence de la note :</strong> Entrez une référence spécifique.</li>
+                                <li><strong>Du / Au :</strong> Sélectionnez une plage de dates.</li>
+                                <li><strong>État de la note :</strong> Choisissez un état (par exemple, "Tous").</li>
+                            </ul>
+                            <p></p>
+
+                            <!-- Section Liste note de frais -->
+                            <h3>Liste des notes de frais</h3>
+                            <p>La liste des notes de frais affiche les informations suivantes :</p>
+                            <ul>
+                                <li><strong>Ref. :</strong> Référence de la note de frais.</li>
+                                <li><strong>Employé :</strong> Nom de l'employé.</li>
+                                <li><strong>Date début / Date fin :</strong> Période couverte par la note de frais.</li>
+                                <li><strong>Montant HT :</strong> Montant hors taxes.</li>
+                                <li><strong>Montant TVA :</strong> Montant de la TVA.</li>
+                                <li><strong>Montant TTC :</strong> Montant total toutes taxes comprises.</li>
+                                <li><strong>État :</strong> État actuel de la note de frais.</li>
+                            </ul>
+
+                            <!-- Section Conseils supplémentaires -->
+                            <h3>Conseils supplémentaires :</h3>
+                            <ul>
+                                <li>Cliquer sur "Afficher toutes les notes de frais" pour voir toutes les notes sans filtre.</li>
+                                <li>Sélectionnez au moins un filtre pour afficher les résultats dans la liste des notes de frais.</li>
+                                <li>Lorsque au moins une note de frais s'affiche, il est possible de cliquer dessus pour afficher son détail.</li>
+                                <li>Cliquez sur le bouton de recherche (loupe) pour appliquer les filtres.</li>
+                                <li>Cliquez sur le bouton de suppression (croix) pour réinitialiser les filtres.</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
